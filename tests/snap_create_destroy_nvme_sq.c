acb5881414971e7c7e88b67cf893d69e0227df33
Max Gurtovoy <maxg@mellanox.com>
2019-12-12 16:48:37 +0200

    tests: add cmdline params for snap_create_destroy_nvme_sq
    usage:
    ./snap_create_destroy_nvme_sq -n <num_queues> -t <type> -d <depth> [-q -m]
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- tests/snap_create_destroy_nvme_sq.c
+++ tests/snap_create_destroy_nvme_sq.c
@@ -8,7 +8,35 @@
 int main(int argc, char **argv)
 {
 	struct ibv_device **list;
-	int ret = 0, i, dev_count;
+	int ret = 0, i, dev_count, opt, num_queues = 10, depth = 16;
+	enum snap_nvme_queue_type q_type = SNAP_NVME_SQE_MODE;
+	bool query = false, modify = false;
+
+	while ((opt = getopt(argc, argv, "mqn:t:d:")) != -1) {
+		switch (opt) {
+		case 'm':
+			modify = true;
+			break;
+		case 'q':
+			query = true;
+			break;
+		case 'n':
+			num_queues = atoi(optarg);
+			break;
+		case 'd':
+			depth = atoi(optarg);
+			break;
+		case 't':
+			if (!strcmp(optarg, "sqe"))
+				q_type = SNAP_NVME_SQE_MODE;
+			else if (!strcmp(optarg, "cc"))
+				q_type = SNAP_NVME_CC_MODE;
+			break;
+		default:
+			printf("Usage: snap_create_destroy_nvme_sq -n <num> -t <type> [mq]\n");
+			exit(1);
+		}
+	}
 
 	list = ibv_get_device_list(&dev_count);
 	if (!list) {
@@ -38,38 +66,60 @@ int main(int argc, char **argv)
 		if (sdev) {
 			ret = snap_nvme_init_device(sdev);
 			if (!ret) {
-				fprintf(stdout, "created NVMe dev for pf %d. creating 16 sqs and cqs\n",
-					attr.pf_id);
+				fprintf(stdout, "created NVMe dev for pf %d. creating %d sqs and cqs type %d and depth %d\n",
+					attr.pf_id, num_queues, q_type, depth);
 				fflush(stdout);
-				for (j = 1; j < 17; j++) {
+				for (j = 1; j < num_queues + 1; j++) {
 					struct snap_nvme_cq_attr cq_attr = {};
 					struct snap_nvme_cq *cq;
 					struct snap_nvme_sq_attr sq_attr = {};
 					struct snap_nvme_sq *sq;
 
-					cq_attr.type = j % 2 ? SNAP_NVME_SQE_MODE : SNAP_NVME_CC_MODE;
+					cq_attr.type = q_type;
 					cq_attr.id = j;
 					cq_attr.doorbell_offset = 4 + j * 8;
 					cq_attr.msix = j;
-					cq_attr.queue_depth = 16;
+					cq_attr.queue_depth = depth;
 					cq_attr.base_addr = 0xdeadbeef * j;
 					cq_attr.cq_period = j * 4;
 					cq_attr.cq_max_count = j * 8;
 					cq = snap_nvme_create_cq(sdev, &cq_attr);
 					if (cq) {
-						sq_attr.type = j % 2 ? SNAP_NVME_SQE_MODE : SNAP_NVME_CC_MODE;
+						sq_attr.type = q_type;
 						sq_attr.id = j;
 						sq_attr.doorbell_offset = j * 8;
-						sq_attr.queue_depth = 16;
+						sq_attr.queue_depth = depth;
 						sq_attr.base_addr = 0xbeefdead * j;
 						sq_attr.cq = cq;
 						sq = snap_nvme_create_sq(sdev, &sq_attr);
 						if (sq) {
-							memset(&sq_attr, 0, sizeof(sq_attr));
-							if (!snap_nvme_query_sq(sq, &sq_attr)) {
-								fprintf(stdout, "Query NVMe sq id=%d, depth=%d\n", j,
-									sq_attr.queue_depth);
-								fflush(stdout);
+							fprintf(stdout, "NVMe sq id=%d created !\n", j);
+							fflush(stdout);
+							if (query) {
+								memset(&sq_attr, 0, sizeof(sq_attr));
+								if (!snap_nvme_query_sq(sq, &sq_attr)) {
+									fprintf(stdout, "Query NVMe sq id=%d, depth=%d\n", j,
+										sq_attr.queue_depth);
+									fflush(stdout);
+								} else {
+									fprintf(stderr, "Failed to Query NVMe sq id=%d\n", j);
+									fflush(stderr);
+								}
+							}
+							if (modify) {
+								memset(&sq_attr, 0, sizeof(sq_attr));
+								sq_attr.state = SNAP_NVME_SQ_STATE_ERR;
+								if (!snap_nvme_modify_sq(sq,
+											 SNAP_NVME_SQ_MOD_STATE,
+											 &sq_attr)) {
+									fprintf(stdout, "Modify NVMe sq id=%d, to state=%d\n", j,
+										sq_attr.state);
+									fflush(stdout);
+								} else {
+									fprintf(stderr, "Failed to Modify NVMe sq id=%d, to state=%d\n", j,
+										sq_attr.state);
+									fflush(stderr);
+								}
 							}
 							snap_nvme_destroy_sq(sq);
 						} else {
