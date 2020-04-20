ec021749bdc59d05089080b90512abee23b6a2c8
Max Gurtovoy <maxg@mellanox.com>
2020-04-20 16:53:45 +0300

    tests: update snap_open_close to use new API
    Use common API for tests to avoid code duplication.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- tests/snap_open_close.c
+++ tests/snap_open_close.c
@@ -3,33 +3,45 @@
 #include <infiniband/verbs.h>
 
 #include "snap.h"
+#include "snap_test.h"
 
 int main(int argc, char **argv)
 {
-	struct ibv_device **list;
+	int type = SNAP_NVME;
+	int opt, ret = 0;
 	struct snap_context *sctx;
-	int ret = 0, i, dev_count;
 
-	list = ibv_get_device_list(&dev_count);
-	if (!list) {
-		fprintf(stderr, "failed to open ib device list.\n");
-		fflush(stderr);
-		ret = 1;
-		goto out;
-	}
-
-	for (i = 0; i < dev_count; i++) {
-		sctx = snap_open(list[i]);
-		if (!sctx) {
-			fprintf(stderr, "failed to create snap ctx for %s err=%d\n",
-				list[i]->name, errno);
-			fflush(stderr);
-			continue;
+	while ((opt = getopt(argc, argv, "t:")) != -1) {
+		switch (opt) {
+		case 't':
+			if (!strcmp(optarg, "all"))
+				type = SNAP_NVME | SNAP_VIRTIO_BLK | SNAP_VIRTIO_NET;
+			else if (!strcmp(optarg, "nvme"))
+				type = SNAP_NVME;
+			else if (!strcmp(optarg, "virtio-blk"))
+				type = SNAP_VIRTIO_BLK;
+			else if (!strcmp(optarg, "virtio-net"))
+				type = SNAP_VIRTIO_NET;
+			break;
+		default:
+			printf("Usage: snap_open_close -t <type: all, nvme, virtio-blk, virtio-net>\n");
+			exit(1);
 		}
-		snap_close(sctx);
 	}
 
-	ibv_free_device_list(list);
+	sctx = snap_ctx_open(type);
+	if (!sctx) {
+		fprintf(stderr, "failed to open snap ctx for %d types\n", type);
+		fflush(stderr);
+		ret = -errno;
+		goto out;
+	} else {
+		fprintf(stdout, "opened snap ctx for %d types on %s\n", type,
+			sctx->context->device->name);
+		fflush(stdout);
+	}
+
+	snap_ctx_close(sctx);
 out:
 	return ret;
 }
