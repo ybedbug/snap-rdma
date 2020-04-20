8b82fecb8f88b7372c1af3596ec94a33e16c1e95
Max Gurtovoy <maxg@mellanox.com>
2020-04-20 16:40:28 +0300

    tests: adapt snap_create_destroy_nvme_cq to use new API
    Use common test API to create/destroy snap context.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- tests/snap_create_destroy_nvme_cq.c
+++ tests/snap_create_destroy_nvme_cq.c
@@ -4,11 +4,15 @@
 
 #include "snap.h"
 #include "snap_nvme.h"
+#include "snap_test.h"
 
 int main(int argc, char **argv)
 {
-	struct ibv_device **list;
-	int ret = 0, i, dev_count, opt, num_queues = 10;
+	struct snap_device_attr attr = {};
+	struct snap_context *sctx;
+	struct snap_device *sdev;
+	int ret = 0, opt, num_queues = 10;
+	int i;
 	enum snap_nvme_queue_type q_type = SNAP_NVME_RAW_MODE;
 
 	while ((opt = getopt(argc, argv, "n:t:")) != -1) {
@@ -28,75 +32,59 @@ int main(int argc, char **argv)
 		}
 	}
 
-	list = ibv_get_device_list(&dev_count);
-	if (!list) {
-		fprintf(stderr, "failed to open ib device list.\n");
+	sctx = snap_ctx_open(SNAP_NVME);
+	if (!sctx) {
+		fprintf(stderr, "failed to open snap ctx for NVMe dev\n");
 		fflush(stderr);
-		ret = 1;
+		ret = -errno;
 		goto out;
 	}
 
-	for (i = 0; i < dev_count; i++) {
-		struct snap_device_attr attr = {};
-		struct snap_context *sctx;
-		struct snap_device *sdev;
-		int j;
+	attr.type = SNAP_NVME_PF;
+	attr.pf_id = 0;
+	sdev = snap_open_device(sctx, &attr);
+	if (sdev) {
+		ret = snap_nvme_init_device(sdev);
+		if (!ret) {
+			fprintf(stdout, "created NVMe dev for pf %d. creating %d cqs type %d\n",
+				attr.pf_id, num_queues, q_type);
+			fflush(stdout);
+			for (i = 1; i < num_queues + 1; i++) {
+				struct snap_nvme_cq_attr cq_attr = {};
+				struct snap_nvme_cq *cq;
 
-		sctx = snap_open(list[i]);
-		if (!sctx) {
-			fprintf(stderr, "failed to create snap ctx for %s err=%d. continue trying\n",
-				list[i]->name, errno);
-			fflush(stderr);
-			continue;
-		}
-
-		attr.type = SNAP_NVME_PF;
-		attr.pf_id = 0;
-		sdev = snap_open_device(sctx, &attr);
-		if (sdev) {
-			ret = snap_nvme_init_device(sdev);
-			if (!ret) {
-				fprintf(stdout, "created NVMe dev for pf %d. creating %d cqs type %d\n",
-					attr.pf_id, num_queues, q_type);
-				fflush(stdout);
-				for (j = 1; j < num_queues + 1; j++) {
-					struct snap_nvme_cq_attr cq_attr = {};
-					struct snap_nvme_cq *cq;
-
-					cq_attr.type = q_type;
-					cq_attr.id = j;
-					cq_attr.msix = j;
-					cq_attr.queue_depth = 16;
-					cq_attr.base_addr = 0xdeadbeef * j;
-					cq_attr.cq_period = j * 4;
-					cq_attr.cq_max_count = j * 8;
-					cq = snap_nvme_create_cq(sdev, &cq_attr);
-					if (cq) {
-						fprintf(stdout, "NVMe cq id=%d created !\n", j);
-						fflush(stdout);
-						snap_nvme_destroy_cq(cq);
-					} else {
-						fprintf(stderr, "failed to create NVMe cq id=%d\n", j);
-						fflush(stderr);
-					}
+				cq_attr.type = q_type;
+				cq_attr.id = i;
+				cq_attr.msix = i;
+				cq_attr.queue_depth = 16;
+				cq_attr.base_addr = 0xdeadbeef * i;
+				cq_attr.cq_period = i * 4;
+				cq_attr.cq_max_count = i * 8;
+				cq = snap_nvme_create_cq(sdev, &cq_attr);
+				if (cq) {
+					fprintf(stdout, "NVMe cq id=%d created !\n", i);
+					fflush(stdout);
+					snap_nvme_destroy_cq(cq);
+				} else {
+					fprintf(stderr, "failed to create NVMe cq id=%d\n", i);
+					fflush(stderr);
 				}
-				snap_nvme_teardown_device(sdev);
-			} else {
-				fprintf(stderr, "failed to create NVMe dev for pf %d ret=%d\n",
-					attr.pf_id, ret);
-				fflush(stderr);
 			}
-			snap_close_device(sdev);
+			snap_nvme_teardown_device(sdev);
 		} else {
-			fprintf(stderr, "failed to create device %d for %s\n",
-				attr.pf_id, list[i]->name);
+			fprintf(stderr, "failed to init NVMe dev for pf %d ret=%d\n",
+				attr.pf_id, ret);
 			fflush(stderr);
 		}
-
-		snap_close(sctx);
+		snap_close_device(sdev);
+	} else {
+		fprintf(stderr, "failed to create device %d for %s\n",
+			attr.pf_id, sctx->context->device->name);
+		fflush(stderr);
+		ret = -errno;
 	}
 
-	ibv_free_device_list(list);
+	snap_ctx_close(sctx);
 out:
 	return ret;
 }
