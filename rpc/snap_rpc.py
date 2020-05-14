3ee3937b94df7baa18769bfc769730e1cc4da6fa
Nitzan Carmi <nitzanc@mellanox.com>
2020-05-14 10:05:45 +0000

    RPC: make snap_rpc.py script standalone
    snap_rpc.py is currently relyting on SPDK scripts
    to run successfully, and specifically "rpc.client"
    which handles RPC communication. Replace this
    dependency with simple internal JSON-RPC client, so
    snap_rpc.py may be run independantely of SPDK.
    
    Signed-off-by: Nitzan Carmi <nitzanc@mellanox.com>

--- rpc/snap_rpc.py
+++ rpc/snap_rpc.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-from rpc.client import print_json, print_dict, JSONRPCException
-
-import logging
 import argparse
-import rpc
 import sys
 import shlex
+import json
+import socket
+import time
+import os
+import copy
 
 try:
     from shlex import quote
@@ -14,19 +15,94 @@ except ImportError:
     from pipes import quote
 
 
-def print_array(a):
-    print(" ".join((quote(v) for v in a)))
+class JsonRpcSnapException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class JsonRpcSnapClient(object):
+    decoder = json.JSONDecoder()
+
+    def __init__(self, sockpath, timeout=60.0):
+        self.sock = None
+        self._request_id = 0
+        self.timeout = timeout
+        try:
+            if os.path.exists(sockpath):
+                self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+                self.sock.connect(sockpath)
+            else:
+                raise socket.error("Unix socket '%s' does not exist" % sockpath)
+        except socket.error as ex:
+            raise JsonRpcSnapException("Error while connecting to %s\n"
+                                       "Error details: %s" % (sockpath, ex))
+
+    def __json_to_string(self, request):
+        return json.dumps(request)
+
+    def send(self, method, params=None):
+        self._request_id += 1
+        req = {
+            'jsonrpc': '2.0',
+            'method': method,
+            'id': self._request_id
+        }
+        if params:
+            req['params'] = copy.deepcopy(params)
+
+        self.sock.sendall(self.__json_to_string(req).encode("utf-8"))
+        return self._request_id
+
+    def __string_to_json(self, request_str):
+        try:
+            obj, idx = self.decoder.raw_decode(request_str)
+            return obj
+        except ValueError:
+            return None
+
+    def recv(self):
+        timeout = self.timeout
+        start_time = time.process_time()
+        response = None
+        buf = ""
+
+        while not response:
+            try:
+                timeout = timeout - (time.process_time() - start_time)
+                self.sock.settimeout(timeout)
+                buf += self.sock.recv(4096).decode("utf-8")
+                response = self.__string_to_json(buf)
+            except socket.timeout:
+                break
+            except ValueError:
+                continue  # incomplete response; keep buffering
+
+        if not response:
+            raise JsonRpcSnapException("Response Timeout")
+        return response
+
+    def call(self, method, params={}):
+        req_id = self.send(method, params)
+        response = self.recv()
+
+        if 'error' in response:
+            params["method"] = method
+            params["req_id"] = req_id
+            msg = "\n".join(["request:", "%s" % json.dumps(params, indent=2),
+                             "Got JSON-RPC error response",
+                             "response:",
+                             json.dumps(response['error'], indent=2)])
+            raise JsonRpcSnapException(msg)
+
+        return response['result']
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Mellanox SNAP JSON-RPC 2.0 command line interface')
     parser.add_argument('-s', dest='server_addr',
-                        help='RPC domain socket path or IP address',
+                        help='RPC domain socket path',
                         default='/var/tmp/spdk.sock')
-    parser.add_argument('-p', dest='port',
-                        help='RPC port number (if server_addr is IP address)',
-                        default=5260, type=int)
     parser.add_argument('-t', dest='timeout',
                         help='Timeout as a floating point number expressed in '
                              'seconds waiting for response. Default: 60.0',
@@ -38,7 +114,8 @@ def main():
                                        dest='called_rpc_name')
 
     def emulation_list(args):
-        print_dict(args.client.call('emulation_list'))
+        result = args.client.call('emulation_list')
+        print(json.dumps(result, indent=2))
     __help = 'List all SNAP plugged emulation functions with their characteristics'
     p = subparsers.add_parser('emulation_list', help=__help)
     p.set_defaults(func=emulation_list)
@@ -56,22 +133,18 @@ def main():
             args.client = client
             try:
                 call_rpc_func(args)
-            except JSONRPCException as ex:
+            except JsonRpcSnapException as ex:
                 print("Exception:")
                 print(executed_rpc.strip() + " <<<")
                 print(ex.message)
                 exit(1)
 
     args = parser.parse_args()
-    args.client = rpc.client.JSONRPCClient(args.server_addr,
-                                           args.port,
-                                           args.timeout,
-                                           log_level=getattr(logging,
-                                                             args.verbose))
+    args.client = JsonRpcSnapClient(args.server_addr, args.timeout)
     if hasattr(args, 'func'):
         try:
             call_rpc_func(args)
-        except JSONRPCException as ex:
+        except JsonRpcSnapException as ex:
             print(ex)
             exit(1)
     elif sys.stdin.isatty():
