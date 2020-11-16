82c55e3b106b2cf8d74b711496c95cff9927cbef
Max Gurtovoy <mgurtovoy@nvidia.com>
2020-11-16 12:00:23 +0000

    tests: add dirty pages test case for channel
    Also add option for verbosity to get more logs in the test.
    
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>

--- tests/snap_open_close_channel.c
+++ tests/snap_open_close_channel.c
@@ -7,7 +7,25 @@ int main(int argc, char **argv)
 {
 	struct snap_channel *schannel;
 	struct snap_migration_ops ops;
-	int ret = 0;
+	int ret = 0, i, opt, num_pages = 100;
+	bool dirty = false, verbose = false;
+
+	while ((opt = getopt(argc, argv, "vdn:")) != -1) {
+		switch (opt) {
+		case 'd':
+			dirty = true;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case 'n':
+			num_pages = atoi(optarg);
+			break;
+		default:
+			printf("Usage: snap_open_close_channel [-d (dirty page logging)] [-v (verbosity)] [-v <num_pages>]\n");
+			exit(1);
+		}
+	}
 
 	schannel = snap_channel_open(&ops, NULL);
 	if (!schannel) {
@@ -17,6 +35,22 @@ int main(int argc, char **argv)
 		goto out;
 	}
 
+	if (dirty) {
+		for (i = 0; i < num_pages; i++) {
+			uint64_t guest_pa = 4096 * (i + 1);
+
+			if (snap_channel_mark_dirty_page(schannel, guest_pa, 4096)) {
+				fprintf(stderr, "failed to mark dirty page 0x%llx\n",
+					guest_pa);
+				fflush(stderr);
+				ret = -EAGAIN;
+			} else if (verbose) {
+				fprintf(stdout, "marked dirty page 0x%llx successfully\n",
+					guest_pa);
+				fflush(stdout);
+			}
+		}
+	}
 	snap_channel_close(schannel);
 
 	fprintf(stdout, "opened and closed snap channel successfully\n");
