ybedbug/snap-rdma

commit e7958d56175e95dd89377cbb549397f1f85516c2

virtio-net: Assign random MAC to VF
VFs don't have valid MAC address when created, assign them random MAC.

Signed-off-by: Bodong Wang <bodong@nvidia.com>

src/snap_virtio_net.c
@@ -61,6 +61,9 @@ int snap_virtio_net_query_device(struct snap_device *sdev,
 			attr->modifiable_fields |= SNAP_VIRTIO_MOD_LINK_STATUS;
 		if (dev_allowed & MLX5_VIRTIO_DEVICE_MODIFY_ENABLED)
 			attr->modifiable_fields |= SNAP_VIRTIO_MOD_ENABLED;
+		if (dev_allowed & MLX5_VIRTIO_DEVICE_MODIFY_DEV_CFG)
+			attr->modifiable_fields |= SNAP_VIRTIO_MOD_DEV_CFG;
+
 	}
 	attr->mtu = DEVX_GET(virtio_net_device_emulation,
 			     device_emulation_out, virtio_net_config.mtu);
@@ -130,6 +133,7 @@ int snap_virtio_net_modify_device(struct snap_device *sdev, uint64_t mask,
  */
 int snap_virtio_net_init_device(struct snap_device *sdev)
 {
+	struct snap_virtio_net_device_attr nattr = {};
 	struct snap_virtio_net_device *vndev;
 	int ret, i;
 
@@ -155,6 +159,19 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 	ret = snap_init_device(sdev);
 	if (ret)
 		goto out_free_virtqs;
+
+	/* Assign random mac to VF*/
+	if (sdev->pci->type == SNAP_VIRTIO_NET_VF) {
+		ret = snap_virtio_net_query_device(sdev, &nattr);
+		if (ret)
+			goto out_free_virtqs;
+		eth_random_addr((uint8_t *)&nattr.mac);
+		ret = snap_virtio_net_modify_device(sdev,
+						    SNAP_VIRTIO_MOD_DEV_CFG,
+						    &nattr);
+		if (ret)
+			goto out_free_virtqs;
+	}
 
 	sdev->dd_data = vndev;
 	vndev->vdev.sdev = sdev;

src/snap_virtio_common.c
@@ -193,6 +193,24 @@ int snap_virtio_modify_device(struct snap_device *sdev,
 			DEVX_SET(virtio_net_device_emulation, device_emulation_in,
 				 enabled, attr->enabled);
 		}
+		if (mask & SNAP_VIRTIO_MOD_DEV_CFG) {
+			fields_to_modify |= MLX5_VIRTIO_DEVICE_MODIFY_DEV_CFG;
+			DEVX_SET(virtio_net_device_emulation,
+				 device_emulation_in,
+				 virtio_net_config.mac_47_16,
+				 nattr->mac >> 16);
+			DEVX_SET(virtio_net_device_emulation,
+				 device_emulation_in,
+				 virtio_net_config.mac_15_0,
+				 nattr->mac & 0xffff);
+			DEVX_SET(virtio_net_device_emulation,
+				 device_emulation_in,
+				 virtio_net_config.max_virtqueue_pairs,
+				 nattr->max_queue_pairs);
+			DEVX_SET(virtio_net_device_emulation,
+				 device_emulation_in,
+				 virtio_net_config.mtu, nattr->mtu);
+		}
 		DEVX_SET64(virtio_net_device_emulation, device_emulation_in,
 			   modify_field_select, fields_to_modify);
 	}

src/snap_virtio_net.h
@@ -114,4 +114,15 @@ to_net_device_attr(struct snap_virtio_device_attr *vattr)
 	return container_of(vattr, struct snap_virtio_net_device_attr, vattr);
 }
 
+static inline void eth_random_addr(uint8_t *addr)
+{
+	uint64_t rand = random();
+
+	rand = rand << 32 | random();
+
+	memcpy(addr, (uint8_t *)&rand, 6);
+	addr[0] &= 0xfe;        /* clear multicast bit */
+	addr[0] |= 0x02;        /* set local assignment bit (IEEE802) */
+}
+
 #endif

commit ad885ab7f3fc50a67aef5cee77494832a40c477f

virtio-net: Genearte valid random MAC address for virtio VF
Driver uses 8 bytes to save MAC address, while the random mac
function only take care of 6 bytes. So, it may generates multicast
mac address.

Fixes: e7958d56175e ("virtio-net: Assign random MAC to VF")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

src/snap_virtio_net.c
@@ -135,6 +135,7 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 {
 	struct snap_virtio_net_device_attr nattr = {};
 	struct snap_virtio_net_device *vndev;
+	uint8_t *mac;
 	int ret, i;
 
 	if (sdev->pci->type != SNAP_VIRTIO_NET_PF &&
@@ -165,7 +166,8 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 		ret = snap_virtio_net_query_device(sdev, &nattr);
 		if (ret)
 			goto out_free_virtqs;
-		eth_random_addr((uint8_t *)&nattr.mac);
+		mac = (uint8_t *)&nattr.mac;
+		eth_random_addr(&mac[2]);
 		ret = snap_virtio_net_modify_device(sdev,
 						    SNAP_VIRTIO_MOD_DEV_CFG,
 						    &nattr);