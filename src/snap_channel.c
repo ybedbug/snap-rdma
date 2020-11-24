8ba30b8cc098e634b3c3ecbe6df8694bdd5390af
Max Gurtovoy <mgurtovoy@nvidia.com>
2020-11-24 15:16:52 +0000

    snap_channel: check ops during channel open
    Fail channel creation in case of missing ops.
    
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>

--- src/snap_channel.c
+++ src/snap_channel.c
@@ -1,3 +1,4 @@
+#include "snap.h"
 #include "snap_channel.h"
 
 int snap_channel_mark_dirty_page(struct snap_channel *schanne, uint64_t guest_pa,
@@ -10,6 +11,19 @@ struct snap_channel *snap_channel_open(struct snap_migration_ops *ops,
 				       void *data)
 {
 	struct snap_channel *schannel;
+
+	if (!ops ||
+	    !ops->quiesce ||
+	    !ops->unquiesce ||
+	    !ops->freeze ||
+	    !ops->unfreeze ||
+	    !ops->get_state_size ||
+	    !ops->copy_state ||
+	    !ops->start_dirty_pages_track ||
+	    !ops->stop_dirty_pages_track) {
+		errno = EINVAL;
+		goto out;
+	}
 
 	schannel = calloc(1, sizeof(*schannel));
 	if (!schannel) {
