4e80adbba00c988f8ac2873a6a00848075d1ee1a
Max Gurtovoy <mgurtovoy@nvidia.com>
2020-11-24 15:18:32 +0000

    tests: add a case for negative open of snap_channel
    Expect to fail in case of missing ops. In this case the test should
    return successful return value since this is expected.
    
    Signed-off-by: Max Gurtovoy <mgurtovoy@nvidia.com>

--- tests/snap_open_close_channel.c
+++ tests/snap_open_close_channel.c
@@ -3,14 +3,67 @@
 #include "snap.h"
 #include "snap_channel.h"
 
+static struct snap_migration_ops test_fail_ops = {};
+
+static int test_quiesce(void *data)
+{
+	return 0;
+}
+
+static int test_unquiesce(void *data)
+{
+	return 0;
+}
+
+static int test_freeze(void *data)
+{
+	return 0;
+}
+
+static int test_unfreeze(void *data)
+{
+	return 0;
+}
+
+static int test_get_state_size(void *data)
+{
+	return 0;
+}
+
+static int test_copy_state(void *data, void *buff, int len, bool copy_from_buffer)
+{
+	return 0;
+}
+
+static int test_start_dirty_pages_track(void *data)
+{
+	return 0;
+}
+
+static int test_stop_dirty_pages_track(void *data)
+{
+	return 0;
+}
+
+static struct snap_migration_ops test_ops = {
+	.quiesce = test_quiesce,
+	.unquiesce = test_unquiesce,
+	.freeze = test_freeze,
+	.unfreeze = test_unfreeze,
+	.get_state_size = test_get_state_size,
+	.copy_state = test_copy_state,
+	.start_dirty_pages_track = test_start_dirty_pages_track,
+	.stop_dirty_pages_track = test_stop_dirty_pages_track,
+};
+
 int main(int argc, char **argv)
 {
 	struct snap_channel *schannel;
-	struct snap_migration_ops ops;
+	struct snap_migration_ops *ops;
 	int ret = 0, i, opt, num_pages = 100;
-	bool dirty = false, verbose = false;
+	bool dirty = false, verbose = false, fail_ops = false;
 
-	while ((opt = getopt(argc, argv, "vdn:")) != -1) {
+	while ((opt = getopt(argc, argv, "vdfn:")) != -1) {
 		switch (opt) {
 		case 'd':
 			dirty = true;
@@ -21,14 +74,27 @@ int main(int argc, char **argv)
 		case 'n':
 			num_pages = atoi(optarg);
 			break;
+		case 'f':
+			fail_ops = true;
+			break;
 		default:
-			printf("Usage: snap_open_close_channel [-d (dirty page logging)] [-v (verbosity)] [-v <num_pages>]\n");
+			printf("Usage: snap_open_close_channel [-d (dirty page logging)] [-v (verbosity)] [-f (fail ops)] [-n <num_pages>]\n");
 			exit(1);
 		}
 	}
 
-	schannel = snap_channel_open(&ops, NULL);
+	if (fail_ops)
+		ops = &test_fail_ops;
+	else
+		ops = &test_ops;
+
+	schannel = snap_channel_open(ops, NULL);
 	if (!schannel) {
+		if (fail_ops) {
+			fprintf(stdout, "failed to open snap channel as expected. Exiting...\n");
+			fflush(stdout);
+			goto out;
+		}
 		fprintf(stderr, "failed to open snap channel\n");
 		fflush(stderr);
 		ret = -errno;
