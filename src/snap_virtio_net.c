ybedbug/snap-rdma

commit ad885ab7f3fc50a67aef5cee77494832a40c477f

virtio-net: Genearte valid random MAC address for virtio VF
Driver uses 8 bytes to save MAC address, while the random mac
function only take care of 6 bytes. So, it may generates multicast
mac address.

Fixes: e7958d56175e ("virtio-net: Assign random MAC to VF")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

src/snap_virtio_net.c
@@ -135,6 +135,7 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 {
 	struct snap_virtio_net_device_attr nattr = {};
 	struct snap_virtio_net_device *vndev;
+	uint8_t *mac;
 	int ret, i;
 
 	if (sdev->pci->type != SNAP_VIRTIO_NET_PF &&
@@ -165,7 +166,8 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 		ret = snap_virtio_net_query_device(sdev, &nattr);
 		if (ret)
 			goto out_free_virtqs;
-		eth_random_addr((uint8_t *)&nattr.mac);
+		mac = (uint8_t *)&nattr.mac;
+		eth_random_addr(&mac[2]);
 		ret = snap_virtio_net_modify_device(sdev,
 						    SNAP_VIRTIO_MOD_DEV_CFG,
 						    &nattr);

commit 392c86a579e1e0c98b1873ba6008f003e2c45325

virtnet: Fix random mac endianness
The masked multicast bit should be on the most significant byte

Fixes: ad885ab7f3fc ("virtio-net: Genearte valid random MAC address for virtio VF")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

src/snap_virtio_net.c
@@ -168,6 +168,7 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 			goto out_free_virtqs;
 		mac = (uint8_t *)&nattr.mac;
 		eth_random_addr(&mac[2]);
+		nattr.mac = be64toh(nattr.mac);
 		ret = snap_virtio_net_modify_device(sdev,
 						    SNAP_VIRTIO_MOD_DEV_CFG,
 						    &nattr);