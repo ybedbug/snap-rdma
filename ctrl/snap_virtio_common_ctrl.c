21f328f9ec2b90cd9c30f7c065231093d714fe09
Nitzan Carmi <nitzanc@nvidia.com>
2020-12-03 18:31:27 +0200

    virtio: Avoid multiple calls to snap_rescan_vfs
    When using sriov, user can decide to handle the
    event himself, otherwise we use gthe default
    of just rescanning the VFs, but surely we shouldnt
    allow doing both.
    
    Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

--- ctrl/snap_virtio_common_ctrl.c
+++ ctrl/snap_virtio_common_ctrl.c
@@ -289,12 +289,12 @@ static int snap_virtio_ctrl_change_num_vfs(const struct snap_virtio_ctrl *ctrl)
 						    ctrl->bar_curr->num_of_vfs);
 		if (ret)
 			return ret;
-	}
-
-	ret = snap_rescan_vfs(ctrl->sdev->pci, ctrl->bar_curr->num_of_vfs);
-	if (ret) {
-		snap_error("Failed to rescan vfs\n");
-		return ret;
+	} else {
+		ret = snap_rescan_vfs(ctrl->sdev->pci, ctrl->bar_curr->num_of_vfs);
+		if (ret) {
+			snap_error("Failed to rescan vfs\n");
+			return ret;
+		}
 	}
 
 	return 0;
