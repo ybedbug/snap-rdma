2b7c12874cb709bca064b71389ccd4758b234a82
Bodong Wang <bodong@mellanox.com>
2020-12-30 14:11:23 +0000

    virtnet: Random MAC should not apply to hotplug device
    Hotplug device gets its MAC from user input of CLI.
    
    Issue: 2403999
    Fixes: 8f38545e3c41 ("virtnet: Fix duplicate MAC addresses for VF")
    Signed-off-by: Bodong Wang <bodong@mellanox.com>

--- src/snap_virtio_net.c
+++ src/snap_virtio_net.c
@@ -161,18 +161,20 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 	if (ret)
 		goto out_free_virtqs;
 
-	/* Assign random mac to virtio net devices */
-	ret = snap_virtio_net_query_device(sdev, &nattr);
-	if (ret)
-		goto out_free_virtqs;
-	mac = (uint8_t *)&nattr.mac;
-	eth_random_addr(&mac[2]);
-	nattr.mac = be64toh(nattr.mac);
-	ret = snap_virtio_net_modify_device(sdev,
-					    SNAP_VIRTIO_MOD_DEV_CFG,
-					    &nattr);
-	if (ret)
-		goto out_free_virtqs;
+	/* Assign random mac to non-hotplug virtio net devices */
+	if (!sdev->pci->hotplug) {
+		ret = snap_virtio_net_query_device(sdev, &nattr);
+		if (ret)
+			goto out_free_virtqs;
+		mac = (uint8_t *)&nattr.mac;
+		eth_random_addr(&mac[2]);
+		nattr.mac = be64toh(nattr.mac);
+		ret = snap_virtio_net_modify_device(sdev,
+						    SNAP_VIRTIO_MOD_DEV_CFG,
+						    &nattr);
+		if (ret)
+			goto out_free_virtqs;
+	}
 
 	sdev->dd_data = vndev;
 	vndev->vdev.sdev = sdev;

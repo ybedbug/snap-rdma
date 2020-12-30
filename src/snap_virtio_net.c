ybedbug/snap-rdma

commit 8f38545e3c4131c0a4b4d8451ac0fbabe455c9d1

virtnet: Fix duplicate MAC addresses for VF
Add random seed to avoid same MAC calculated.
Also use randomized MAC address for PF device.

Issue: 2402746
Signed-off-by: Hong Xu <hongx@nvidia.com>

src/snap_virtio_net.c
@@ -161,20 +161,18 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 	if (ret)
 		goto out_free_virtqs;
 
-	/* Assign random mac to VF*/
-	if (sdev->pci->type == SNAP_VIRTIO_NET_VF) {
-		ret = snap_virtio_net_query_device(sdev, &nattr);
-		if (ret)
-			goto out_free_virtqs;
-		mac = (uint8_t *)&nattr.mac;
-		eth_random_addr(&mac[2]);
-		nattr.mac = be64toh(nattr.mac);
-		ret = snap_virtio_net_modify_device(sdev,
-						    SNAP_VIRTIO_MOD_DEV_CFG,
-						    &nattr);
-		if (ret)
-			goto out_free_virtqs;
-	}
+	/* Assign random mac to virtio net devices */
+	ret = snap_virtio_net_query_device(sdev, &nattr);
+	if (ret)
+		goto out_free_virtqs;
+	mac = (uint8_t *)&nattr.mac;
+	eth_random_addr(&mac[2]);
+	nattr.mac = be64toh(nattr.mac);
+	ret = snap_virtio_net_modify_device(sdev,
+					    SNAP_VIRTIO_MOD_DEV_CFG,
+					    &nattr);
+	if (ret)
+		goto out_free_virtqs;
 
 	sdev->dd_data = vndev;
 	vndev->vdev.sdev = sdev;

src/snap_virtio_net.h
@@ -33,6 +33,7 @@
 #ifndef SNAP_VIRTIO_NET_H
 #define SNAP_VIRTIO_NET_H
 
+#include <sys/time.h>
 #include "snap.h"
 #include "snap_virtio_common.h"
 
@@ -117,6 +118,9 @@ to_net_device_attr(struct snap_virtio_device_attr *vattr)
 
 static inline void eth_random_addr(uint8_t *addr)
 {
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	srandom(t.tv_sec + t.tv_usec);
 	uint64_t rand = random();
 
 	rand = rand << 32 | random();

commit 2b7c12874cb709bca064b71389ccd4758b234a82

virtnet: Random MAC should not apply to hotplug device
Hotplug device gets its MAC from user input of CLI.

Issue: 2403999
Fixes: 8f38545e3c41 ("virtnet: Fix duplicate MAC addresses for VF")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

src/snap_virtio_net.c
@@ -161,18 +161,20 @@ int snap_virtio_net_init_device(struct snap_device *sdev)
 	if (ret)
 		goto out_free_virtqs;
 
-	/* Assign random mac to virtio net devices */
-	ret = snap_virtio_net_query_device(sdev, &nattr);
-	if (ret)
-		goto out_free_virtqs;
-	mac = (uint8_t *)&nattr.mac;
-	eth_random_addr(&mac[2]);
-	nattr.mac = be64toh(nattr.mac);
-	ret = snap_virtio_net_modify_device(sdev,
-					    SNAP_VIRTIO_MOD_DEV_CFG,
-					    &nattr);
-	if (ret)
-		goto out_free_virtqs;
+	/* Assign random mac to non-hotplug virtio net devices */
+	if (!sdev->pci->hotplug) {
+		ret = snap_virtio_net_query_device(sdev, &nattr);
+		if (ret)
+			goto out_free_virtqs;
+		mac = (uint8_t *)&nattr.mac;
+		eth_random_addr(&mac[2]);
+		nattr.mac = be64toh(nattr.mac);
+		ret = snap_virtio_net_modify_device(sdev,
+						    SNAP_VIRTIO_MOD_DEV_CFG,
+						    &nattr);
+		if (ret)
+			goto out_free_virtqs;
+	}
 
 	sdev->dd_data = vndev;
 	vndev->vdev.sdev = sdev;