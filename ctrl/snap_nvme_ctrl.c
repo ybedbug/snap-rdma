0a67aaf665303cb93bd2e174bedc84b3460b2644
Nitzan Carmi <nitzanc@nvidia.com>
2021-07-18 17:23:28 +0000

    LICENSE: Change file to be NVIDIA license
    Change all files in the repository to have the NVIDIA
    proprietary license.
    
    Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

--- ctrl/snap_nvme_ctrl.c
+++ ctrl/snap_nvme_ctrl.c
@@ -1,3 +1,15 @@
+/*
+ * Copyright © 2021 NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of Nvidia Corporation and its affiliates
+ * (the "Company") and all right, title, and interest in and to the software
+ * product, including all associated intellectual property rights, are and
+ * shall remain exclusively with the Company.
+ *
+ * This software product is governed by the End User License Agreement
+ * provided with the software product.
+ */
+
 #include "snap_nvme_ctrl.h"
 
 static int snap_nvme_ctrl_bar_read(struct snap_nvme_ctrl *ctrl)
--- ctrl/snap_virtio_blk_virtq.c
+++ ctrl/snap_virtio_blk_virtq.c
@@ -1,3 +1,15 @@
+/*
+ * Copyright © 2021 NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+ *
+ * This software product is a proprietary product of Nvidia Corporation and its affiliates
+ * (the "Company") and all right, title, and interest in and to the software
+ * product, including all associated intellectual property rights, are and
+ * shall remain exclusively with the Company.
+ *
+ * This software product is governed by the End User License Agreement
+ * provided with the software product.
+ */
+
 #include <linux/virtio_ring.h>
 #include <linux/virtio_blk.h>
 #include <linux/virtio_pci.h>
