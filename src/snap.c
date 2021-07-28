ybedbug/snap-rdma

commit 7be1e74572d1881bc53c4670a3a9f0af0bc7cf72

nvme: Check for HW queue depth capacity
When opening a CQ/SQ, make sure HW supports
such queue depth.

Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

src/snap.c
@@ -26,6 +26,7 @@
 #define SNAP_PCI_ENUMERATE_TIME_WAIT 50000
 #define SNAP_PCI_ENUMERATE_MAX_RETRIES 100
 #define SNAP_UNINITIALIZED_VHCA_ID -1
+#define SNAP_NVME_MAX_QUEUE_DEPTH_LEGACY 2048
 
 static int snap_copy_roce_address(struct snap_device *sdev,
 		struct ibv_context *context, int idx);
@@ -808,6 +809,10 @@ static int snap_query_nvme_emulation_caps(struct snap_context *sctx)
 		capability.nvme_emulation_cap.log_max_emulated_cq);
 	sctx->nvme_caps.max_emulated_nvme_sqs = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_emulated_sq);
+	sctx->nvme_caps.max_queue_depth = 1 << DEVX_GET(query_hca_cap_out, out,
+		capability.nvme_emulation_cap.log_max_queue_depth);
+	if (sctx->nvme_caps.max_queue_depth == 0)
+		sctx->nvme_caps.max_queue_depth = SNAP_NVME_MAX_QUEUE_DEPTH_LEGACY;
 	sctx->nvme_caps.cq_interrupt_disabled = DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.nvme_cq_interrupt_disabled);
 	sctx->nvme_caps.reg_size = DEVX_GET(query_hca_cap_out, out,

src/mlx5_ifc.h
@@ -1422,7 +1422,8 @@ struct mlx5_ifc_nvme_emulation_cap_bits {
 	u8	 reserved_at_20[0x10];
 	u8	 registers_size[0x10];
 
-	u8	 reserved_at_40[0x13];
+	u8	 log_max_queue_depth[0x10];
+	u8	 reserved_at_40[0x3];
 	u8	 log_max_emulated_sq[0x5];
 	u8	 reserved_at_58[0x3];
 	u8	 log_max_emulated_cq[0x5];

src/snap.h
@@ -349,6 +349,7 @@ struct snap_nvme_caps {
 	uint32_t	max_nvme_namespaces;
 	uint32_t	max_emulated_nvme_cqs;
 	uint32_t	max_emulated_nvme_sqs;
+	uint32_t	max_queue_depth;
 	bool		cq_interrupt_disabled;
 	uint16_t	reg_size;
 	bool		crossing_vhca_mkey;

src/snap_nvme.c
@@ -467,6 +467,11 @@ snap_nvme_create_cq(struct snap_device *sdev, struct snap_nvme_cq_attr *attr)
 		goto out;
 	}
 
+	if (attr->queue_depth > hw_caps->max_queue_depth) {
+		errno = ENOTSUP;
+		goto out;
+	}
+
 	cq = &ndev->cqs[attr->id];
 
 	DEVX_SET(general_obj_in_cmd_hdr, in, opcode,
@@ -885,6 +890,7 @@ snap_nvme_create_sq(struct snap_device *sdev, struct snap_nvme_sq_attr *attr)
 	uint8_t *sq_in;
 	struct snap_nvme_sq *sq;
 	int offload_type;
+	const struct snap_nvme_caps *hw_caps = &sdev->sctx->nvme_caps;
 
 	if (attr->type == SNAP_NVME_RAW_MODE) {
 		offload_type = MLX5_NVME_SQ_OFFLOAD_TYPE_SQE;
@@ -897,6 +903,11 @@ snap_nvme_create_sq(struct snap_device *sdev, struct snap_nvme_sq_attr *attr)
 
 	if (attr->id >= ndev->num_queues) {
 		errno = EINVAL;
+		goto out;
+	}
+
+	if (attr->queue_depth > hw_caps->max_queue_depth) {
+		errno = ENOTSUP;
 		goto out;
 	}
 

commit 5dfb76a5eee5b2c9bb0204c27a55d6ffdebb4856

nvme: Fix Queue Depth default assignment
Since FW gives us the value in log, then in legacy
mode log value is 0, which means the queue_depth
is 1 (not 0).

Fixes: 7be1e74 nvme: Check for HW queue depth capacity

Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

src/snap.c
@@ -815,7 +815,7 @@ static int snap_query_nvme_emulation_caps(struct snap_context *sctx)
 		capability.nvme_emulation_cap.log_max_emulated_sq);
 	sctx->nvme_caps.max_queue_depth = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_queue_depth);
-	if (sctx->nvme_caps.max_queue_depth == 0)
+	if (sctx->nvme_caps.max_queue_depth == 1)
 		sctx->nvme_caps.max_queue_depth = SNAP_NVME_MAX_QUEUE_DEPTH_LEGACY;
 	sctx->nvme_caps.cq_interrupt_disabled = DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.nvme_cq_interrupt_disabled);