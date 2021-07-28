ybedbug/snap-rdma

commit b9c8ac91541822b9ff7b471a31615556bab4253f

nvme: Check for maximal nsid limitation
When creating nvme namespace, make sure its
src_nsid does not exceed maximal nsid supported
by the HW.

Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

src/snap.c
@@ -805,6 +805,10 @@ static int snap_query_nvme_emulation_caps(struct snap_context *sctx)
 			 capability.nvme_emulation_cap.max_emulated_devices);
 	sctx->nvme_caps.max_nvme_namespaces = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_nvme_offload_namespaces);
+	sctx->nvme_caps.max_nvme_nsid = 1 << DEVX_GET(query_hca_cap_out, out,
+		capability.nvme_emulation_cap.log_max_nvme_offload_nsid);
+	if (sctx->nvme_caps.max_nvme_nsid == 0)
+		sctx->nvme_caps.max_nvme_nsid = sctx->nvme_caps.max_nvme_namespaces;
 	sctx->nvme_caps.max_emulated_nvme_cqs = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_emulated_cq);
 	sctx->nvme_caps.max_emulated_nvme_sqs = 1 << DEVX_GET(query_hca_cap_out, out,

src/mlx5_ifc.h
@@ -1419,7 +1419,8 @@ struct mlx5_ifc_nvme_emulation_cap_bits {
 	u8	 reserved_at_10[0x7];
 	u8	 max_emulated_devices[0x10];
 
-	u8	 reserved_at_20[0x10];
+	u8	 reserved_at_20[0xb];
+	u8	 log_max_nvme_offload_nsid[0x5];
 	u8	 registers_size[0x10];
 
 	u8	 log_max_queue_depth[0x10];

src/snap.h
@@ -347,6 +347,7 @@ struct snap_pfs_ctx {
 struct snap_nvme_caps {
 	int		supported_types;//mask of snap_nvme_queue_type
 	uint32_t	max_nvme_namespaces;
+	uint32_t	max_nvme_nsid;
 	uint32_t	max_emulated_nvme_cqs;
 	uint32_t	max_emulated_nvme_sqs;
 	uint32_t	max_queue_depth;

src/snap_nvme.c
@@ -338,6 +338,12 @@ snap_nvme_create_namespace(struct snap_device *sdev,
 	struct snap_nvme_device *ndev = (struct snap_nvme_device *)sdev->dd_data;
 	uint8_t *namespace_in;
 	struct snap_nvme_namespace *ns;
+	const struct snap_nvme_caps *hw_caps = &sdev->sctx->nvme_caps;
+
+	if (attr->src_nsid > hw_caps->max_nvme_nsid) {
+		errno = ENOTSUP;
+		goto out;
+	}
 
 	ns = calloc(1, sizeof(*ns));
 	if (!ns) {

commit 4bbe1e65af577f6b8c0860acc2411480a8baafaa

nvme: Fix Max NSID default assignment
Since FW gives us the value in log, then in legacy
mode log value is 0, which means the max_nsid
is 1 (not 0).

Fixes: b9c8ac9 nvme: Check for maximal nsid limitation

Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

src/snap.c
@@ -807,7 +807,7 @@ static int snap_query_nvme_emulation_caps(struct snap_context *sctx)
 		capability.nvme_emulation_cap.log_max_nvme_offload_namespaces);
 	sctx->nvme_caps.max_nvme_nsid = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_nvme_offload_nsid);
-	if (sctx->nvme_caps.max_nvme_nsid == 0)
+	if (sctx->nvme_caps.max_nvme_nsid == 1)
 		sctx->nvme_caps.max_nvme_nsid = sctx->nvme_caps.max_nvme_namespaces;
 	sctx->nvme_caps.max_emulated_nvme_cqs = 1 << DEVX_GET(query_hca_cap_out, out,
 		capability.nvme_emulation_cap.log_max_emulated_cq);