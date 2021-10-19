61d570d5f4af5531f4528f895ebb88bc63e4403f
Hong Xu <hongx@nvidia.com>
2021-10-19 05:16:07 -0500

    virtio: Quit FLR on errors in pre_flr
    pre_flr returns 0 on success, others on failure
    
    Signed-off-by: Hong Xu <hongx@nvidia.com>

--- ctrl/snap_virtio_common_ctrl.c
+++ ctrl/snap_virtio_common_ctrl.c
@@ -421,7 +421,7 @@ static int snap_virtio_ctrl_change_status(struct snap_virtio_ctrl *ctrl)
 		}
 
 		if (ctrl->bar_cbs.pre_flr) {
-			if (!ctrl->bar_cbs.pre_flr(ctrl->cb_ctx))
+			if (ctrl->bar_cbs.pre_flr(ctrl->cb_ctx))
 				return 0;
 		}
 		snap_close_device(ctrl->sdev);
