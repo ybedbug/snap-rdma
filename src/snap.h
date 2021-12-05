b070409c9a10bea30084764945da1ecf3dc79057
Nitzan Carmi <nitzanc@nvidia.com>
2021-12-05 09:03:29 +0200

    snap: Introduce snap_macros.h
    The new header should group all snap special defines
    and macros assistances.
    
    Signed-off-by: Nitzan Carmi <nitzanc@nvidia.com>

--- src/snap.h
+++ src/snap.h
@@ -31,28 +31,9 @@
 #include "snap_mr.h"
 #include "snap_crypto.h"
 #include "mlx5_snap.h"
+#include "snap_macros.h"
 
 #define PFX "snap: "
-
-#ifndef offsetof
-#define offsetof(t, m) ((size_t) &((t *)0)->m)
-#endif
-
-#ifndef container_of
-#define container_of(ptr, type, member) ({ \
-		typeof(((type *)0)->member)*__mptr = (ptr); \
-		(type *)((char *)__mptr - offsetof(type, member)); })
-#endif
-
-#define snap_min(a, b) (((a) < (b)) ? (a) : (b))
-#define snap_max(a, b) (((a) > (b)) ? (a) : (b))
-
-#define snap_likely(x) __builtin_expect(!!(x), 1)
-#define snap_unlikely(x) __builtin_expect(!!(x), 0)
-
-#ifndef ARRAY_SIZE
-#define ARRAY_SIZE(n) (sizeof(n) / sizeof(n[0]))
-#endif
 
 #ifndef SNAP_DEBUG
 #define SNAP_DEBUG 0
@@ -403,30 +384,6 @@ enum  mlx5_emulation_hotplug_state {
 	MLX5_EMULATION_HOTPLUG_STATE_POWER_OFF = 3,
 	MLX5_EMULATION_HOTPLUG_STATE_POWER_ON = 4,
 };
-
-#define SNAP_ALIGN_FLOOR(val, align) \
-	(typeof(val))((val) & (~((typeof(val))((align) - 1))))
-
-#define SNAP_ALIGN_CEIL(val, align) \
-	SNAP_ALIGN_FLOOR(((val) + ((typeof(val)) (align) - 1)), align)
-
-#define SNAP_IS_POW2_OR_ZERO(_n) (!((_n) & ((_n) - 1)))
-
-#define SNAP_IS_POW2(_n) (((_n) > 0) && SNAP_IS_POW2_OR_ZERO(_n))
-
-#define SNAP_ROUNDUP_POW2(_n) \
-	({ \
-	 /* a hack to discard 'const' qualifier of _n */\
-	typeof((_n) + 0) pow2; \
-	/* weird indentation here to make style check happy */\
-	for \
-	 (pow2 = 1; pow2 < (_n); pow2 <<= 1); \
-	\
-	pow2; \
-	})
-
-#define SNAP_ROUNDUP_POW2_OR0(_n) \
-	(((_n) == 0) ? 0 : SNAP_ROUNDUP_POW2(_n))
 
 #define SNAP_KLM_MAX_TRANSLATION_ENTRIES_NUM   128
 
