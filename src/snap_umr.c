ef3ff7985bacfbff7fe8ef34cb3cecad8a13bedb
Tom Wu <tomwu@nvidia.com>
2021-12-09 05:25:30 +0200

    snap_umr: extend snap_umr_post_wqe to support attach cryptoi bsf
    Extend snap_umr_post_wqe API to support
    attach crypto bsf to a mkey.
    After this commit, snap_umr_post_wqe can post UMR to do:
      1. attach MTT to mkey
      2. attach Crypto BSF to mkey
      3. attach MTT and Crypto BSF both to mkey
    
    Signed-off-by: Tom Wu <tomwu@nvidia.com>

--- src/snap_umr.c
+++ src/snap_umr.c
@@ -24,142 +24,127 @@ static inline void set_umr_inline_klm_seg(union mlx5_wqe_umr_inline_seg *klm,
 }
 
 static inline void set_umr_mkey_seg_mtt(struct mlx5_wqe_mkey_context_seg *mkey,
-					struct mlx5_klm *klm_mtt, int klm_entries)
+					struct snap_post_umr_attr *attr)
 {
 	int i;
 	uint64_t len = 0;
 
 	mkey->free = 0;
-	mkey->start_addr = htobe64(klm_mtt[0].address);
+	/* modify start_addr when it is a IOV type IO */
+	if (attr->purpose == SNAP_UMR_MKEY_MODIFY_ATTACH_MTT)
+		mkey->start_addr = htobe64(attr->klm_mtt[0].address);
 
-	for (i = 0; i < klm_entries; i++)
-		len += klm_mtt[i].byte_count;
+	for (i = 0; i < attr->klm_entries; i++)
+		len += attr->klm_mtt[i].byte_count;
 
 	mkey->len = htobe64(len);
 }
 
-static inline void set_umr_control_seg_mtt(struct mlx5_wqe_umr_ctrl_seg *ctrl,
-					int klm_entries)
+static inline void set_umr_ctrl_seg_mtt(struct mlx5_wqe_umr_ctrl_seg *ctrl,
+					struct snap_post_umr_attr *attr)
 {
-	/* explicitly set rsvd0 and rsvd1 from struct mlx5_wqe_umr_ctrl_seg to 0,
-	 * otherwise post umr wqe will fail if reuse those WQE BB with dirty data.
-	 **/
-	*(uint32_t *)ctrl = 0;
-	*((uint64_t *)ctrl + 2) = 0;
-	*((uint64_t *)ctrl + 3) = 0;
-	*((uint64_t *)ctrl + 4) = 0;
-	*((uint64_t *)ctrl + 5) = 0;
+	uint64_t mkey_mask;
 
-	ctrl->flags = MLX5_WQE_UMR_CTRL_FLAG_INLINE |
-				MLX5_WQE_UMR_CTRL_FLAG_TRNSLATION_OFFSET;
+	ctrl->flags |= MLX5_WQE_UMR_CTRL_FLAG_INLINE;
 
-	/* if use a non-zero offset value, should use htobe16(offset) */
-	ctrl->translation_offset = 0;
-
-	ctrl->klm_octowords = htobe16(SNAP_ALIGN_CEIL(klm_entries, 4));
+	ctrl->klm_octowords = htobe16(SNAP_ALIGN_CEIL(attr->klm_entries, 4));
 
 	/*
 	 * Going to modify three properties of KLM mkey:
 	 *  1. 'free' field: change this mkey from in free to in use
 	 *  2. 'len' field: to include the total bytes in iovec
 	 *  3. 'start_addr' field: use the address of first element as
-	 *       the start_addr of this mkey
+	 *       the start_addr of this mkey. (ONLY for IOV type IO)
 	 **/
-	ctrl->mkey_mask = htobe64(MLX5_WQE_UMR_CTRL_MKEY_MASK_FREE |
-				MLX5_WQE_UMR_CTRL_MKEY_MASK_LEN |
-				MLX5_WQE_UMR_CTRL_MKEY_MASK_START_ADDR);
+	mkey_mask = MLX5_WQE_UMR_CTRL_MKEY_MASK_FREE
+				| MLX5_WQE_UMR_CTRL_MKEY_MASK_LEN;
+	if (attr->purpose == SNAP_UMR_MKEY_MODIFY_ATTACH_MTT)
+		mkey_mask |= MLX5_WQE_UMR_CTRL_MKEY_MASK_START_ADDR;
+
+	ctrl->mkey_mask |= htobe64(mkey_mask);
 }
 
-static int build_inline_mtt_umr_wqe(struct snap_dma_q *q,
-			struct mlx5_wqe_ctrl_seg *ctrl, uint8_t fm_ce_se,
-			uint32_t to_end, struct snap_post_umr_attr *attr,
-			uint32_t *umr_wqe_n_bb)
+static inline void set_umr_crypto_bsf_seg(struct snap_crypto_bsf_seg *bsf,
+			struct snap_post_umr_attr *attr)
 {
-	struct snap_dv_qp *dv_qp;
-	struct mlx5_wqe_ctrl_seg *gen_ctrl;
-	struct mlx5_wqe_umr_ctrl_seg *umr_ctrl;
-	struct mlx5_wqe_mkey_context_seg *mkey;
-	union mlx5_wqe_umr_inline_seg *klm;
-	uint32_t wqe_size, inline_klm_size;
-	uint32_t i, translation_size;
+	/* explicitly set rsvd0/rsvd1/rsvd2/rsvd3 to 0 */
+	*((uint64_t *)bsf) = 0;
+	*((uint64_t *)bsf + 1) = 0;
+	*((uint64_t *)bsf + 4) = 0;
+	*((uint64_t *)bsf + 6) = 0;
+	*((uint64_t *)bsf + 7) = 0;
 
-	if (attr->klm_mtt == NULL || attr->klm_entries == 0)
-		return -EINVAL;
+	bsf->size_type = (SNAP_CRYPTO_BSF_SIZE_64B << 6)
+					| SNAP_CRYPTO_BSF_P_TYPE_CRYPTO;
+	bsf->enc_order = attr->encryption_order;
+	bsf->enc_standard = attr->encryption_standard;
+	bsf->raw_data_size = htobe32(attr->raw_data_size);
+	bsf->crypto_block_size_pointer = attr->crypto_block_size_pointer;
+	bsf->dek_pointer = htobe32(attr->dek_pointer & 0x00FFFFFF);
+	memcpy(bsf->xts_initial_tweak, attr->xts_initial_tweak,
+		SNAP_CRYPTO_XTS_INITIAL_TWEAK_SIZE);
+	memcpy(bsf->keytag, attr->keytag, SNAP_CRYPTO_KEYTAG_SIZE);
+}
 
-	dv_qp = &q->sw_qp.dv_qp;
+static inline void set_umr_mkey_seg_crypto_bsf(
+					struct mlx5_wqe_mkey_context_seg *mkey,
+					int bsf_size)
+{
+	mkey->bsf_octword_size = htobe32(SNAP_ALIGN_CEIL(round_up(bsf_size, 16), 4));
+}
+
+static inline void set_umr_ctrl_seg_crypto_bsf(
+					struct mlx5_wqe_umr_ctrl_seg *ctrl,
+					int bsf_size)
+{
+	ctrl->flags |= MLX5_WQE_UMR_CTRL_FLAG_INLINE;
+
+	ctrl->bsf_octowords = htobe16(SNAP_ALIGN_CEIL(round_up(bsf_size, 16), 4));
 
 	/*
-	 * UMR WQE LAYOUT:
-	 * -------------------------------------------------------
-	 * | gen_ctrl | umr_ctrl | mkey_ctx |   inline klm mtt   |
-	 * -------------------------------------------------------
-	 *   16bytes    48bytes    64bytes    num_mtt * 16 bytes
-	 *
-	 * Note: size of inline klm mtt should be aligned to 64 bytes.
-	 */
-	translation_size = SNAP_ALIGN_CEIL(attr->klm_entries, 4);
-	inline_klm_size = translation_size * sizeof(*klm);
-	wqe_size = sizeof(*gen_ctrl) + sizeof(*umr_ctrl) +
-		sizeof(*mkey) + inline_klm_size;
-
-	*umr_wqe_n_bb = round_up(wqe_size, MLX5_SEND_WQE_BB);
-
-	/*
-	 * umr wqe only do the modification to klm-mkey,
-	 * and there will be one RDMA/GGA-READ/WEIR wqe
-	 * followed right after to use this modified klm-key.
-	 *
-	 * A .readv()/.writev() consider process succeed
-	 * only when both umr wqe and RDMA/GGA-READ/WEIR
-	 * wqes post succeed.
-	 *
-	 * In order to avoid post UMR WQE succeed but lack
-	 * of tx available resource to post followed DMA WQE
-	 * case, use umr_wqe_n_bb + 1 to do the can_tx check.
-	 */
-	if (snap_unlikely(!qp_can_tx(q, *umr_wqe_n_bb + 1)))
-		return -EAGAIN;
-
-	/* sizeof(gen_ctrl) + sizeof(umr_ctrl) == MLX5_SEND_WQE_BB,
-	 * so do not need to worry about wqe buffer warp around.
-	 * build genenal ctrl segment
+	 * Going to modify one propertie of cross mkey:
+	 *  1. 'bsf_octoword_size' field
 	 **/
-	gen_ctrl = ctrl;
-	snap_set_ctrl_seg(gen_ctrl, dv_qp->hw_qp.sq.pi, MLX5_OPCODE_UMR, 0,
-				dv_qp->hw_qp.qp_num, fm_ce_se,
-				round_up(wqe_size, 16), 0,
-				htobe32(attr->klm_mkey->mkey));
+	ctrl->mkey_mask |= htobe64(SNAP_WQE_UMR_CTRL_MKEY_MASK_BSF_OCTOWORD_SIZE);
+}
 
-	/* build umr ctrl segment */
-	umr_ctrl = (struct mlx5_wqe_umr_ctrl_seg *)(gen_ctrl + 1);
-	set_umr_control_seg_mtt(umr_ctrl, attr->klm_entries);
-
-	/* build mkey context segment */
-	to_end -= MLX5_SEND_WQE_BB;
-	if (to_end == 0) { /* wqe buffer wap around */
-		mkey = (struct mlx5_wqe_mkey_context_seg *)(dv_qp->hw_qp.sq.addr);
-		to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
-	} else {
-		mkey = (struct mlx5_wqe_mkey_context_seg *)(umr_ctrl + 1);
+static void snap_build_inline_crypto_bsf(uint32_t *to_end,
+				struct snap_crypto_bsf_seg *bsf,
+				struct snap_dv_qp *dv_qp,
+				struct snap_post_umr_attr *attr)
+{
+	/* build inline crypro bsf */
+	if (*to_end == 0) { /* wqe buffer wap around */
+		bsf = (struct snap_crypto_bsf_seg *) (dv_qp->hw_qp.sq.addr);
+		*to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
 	}
-	set_umr_mkey_seg_mtt(mkey, attr->klm_mtt, attr->klm_entries);
+
+	set_umr_crypto_bsf_seg(bsf, attr);
+	*to_end -= MLX5_SEND_WQE_BB;
+}
+
+static void *snap_build_inline_mtt(uint32_t *to_end,
+				union mlx5_wqe_umr_inline_seg *klm,
+				struct snap_dv_qp *dv_qp,
+				struct snap_post_umr_attr *attr)
+{
+	int i, translation_size;
+
+	translation_size = SNAP_ALIGN_CEIL(attr->klm_entries, 4);
 
 	/* build inline mtt entires */
-	to_end -= MLX5_SEND_WQE_BB;
-	if (to_end == 0) { /* wqe buffer wap around */
+	if (*to_end == 0) { /* wqe buffer warp around */
 		klm = (union mlx5_wqe_umr_inline_seg *) (dv_qp->hw_qp.sq.addr);
-		to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
-	} else {
-		klm = (union mlx5_wqe_umr_inline_seg *)(mkey + 1);
+		*to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
 	}
 
 	for (i = 0; i < attr->klm_entries; i++) {
 		set_umr_inline_klm_seg(klm, &attr->klm_mtt[i]);
 		/* sizeof(*klm) * 4 == MLX5_SEND_WQE_BB */
-		to_end -= sizeof(union mlx5_wqe_umr_inline_seg);
-		if (to_end == 0) { /* wqe buffer wap around */
+		*to_end -= sizeof(union mlx5_wqe_umr_inline_seg);
+		if (*to_end == 0) { /* wqe buffer warp around */
 			klm = (union mlx5_wqe_umr_inline_seg *) (dv_qp->hw_qp.sq.addr);
-			to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
+			*to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
 		} else {
 			klm = klm + 1;
 		}
@@ -171,10 +156,156 @@ static int build_inline_mtt_umr_wqe(struct snap_dma_q *q,
 	 **/
 	for (; i < translation_size; i++) {
 		memset(klm, 0, sizeof(*klm));
+		*to_end -= sizeof(union mlx5_wqe_umr_inline_seg);
 		klm = klm + 1;
 	}
 
 	attr->klm_mkey->addr = attr->klm_mtt[0].address;
+
+	return (void *)klm;
+}
+
+static void snap_set_umr_mkey_seg(struct mlx5_wqe_mkey_context_seg *mkey,
+			struct snap_post_umr_attr *attr)
+{
+	/* explicitly set all struct mlx5_wqe_mkey_context_seg to 0 */
+	*((uint64_t *)mkey) = 0;
+	*((uint64_t *)mkey + 1) = 0;
+	*((uint64_t *)mkey + 2) = 0;
+	*((uint64_t *)mkey + 3) = 0;
+	*((uint64_t *)mkey + 4) = 0;
+	*((uint64_t *)mkey + 5) = 0;
+	*((uint64_t *)mkey + 6) = 0;
+	*((uint64_t *)mkey + 7) = 0;
+
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_MTT)
+		set_umr_mkey_seg_mtt(mkey, attr);
+
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_CRYPTO_BSF)
+		set_umr_mkey_seg_crypto_bsf(mkey, sizeof(struct snap_crypto_bsf_seg));
+}
+
+static void snap_set_umr_ctrl_seg(struct mlx5_wqe_umr_ctrl_seg *umr_ctrl,
+			struct snap_post_umr_attr *attr)
+{
+	/* explicitly set rsvd0 and rsvd1 from struct mlx5_wqe_umr_ctrl_seg to 0,
+	 * otherwise post umr wqe will fail if reuse those WQE BB with dirty data.
+	 **/
+	*(uint64_t *)umr_ctrl = 0;
+	*((uint64_t *)umr_ctrl + 1) = 0;
+	*((uint64_t *)umr_ctrl + 2) = 0;
+	*((uint64_t *)umr_ctrl + 3) = 0;
+	*((uint64_t *)umr_ctrl + 4) = 0;
+	*((uint64_t *)umr_ctrl + 5) = 0;
+
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_MTT)
+		set_umr_ctrl_seg_mtt(umr_ctrl, attr);
+
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_CRYPTO_BSF)
+		set_umr_ctrl_seg_crypto_bsf(umr_ctrl, sizeof(struct snap_crypto_bsf_seg));
+}
+
+static int snap_build_umr_wqe(struct snap_dma_q *q,
+			struct mlx5_wqe_ctrl_seg *ctrl, uint8_t fm_ce_se,
+			uint32_t to_end, struct snap_post_umr_attr *attr,
+			uint32_t *umr_wqe_n_bb)
+{
+	struct snap_dv_qp *dv_qp;
+	struct mlx5_wqe_ctrl_seg *gen_ctrl;
+	struct mlx5_wqe_umr_ctrl_seg *umr_ctrl;
+	struct mlx5_wqe_mkey_context_seg *mkey;
+	union mlx5_wqe_umr_inline_seg *klm;
+	struct snap_crypto_bsf_seg *bsf;
+	void *inline_mtt_end;
+	uint32_t wqe_size, translation_size;
+	uint32_t inline_klm_size = 0;
+
+	if ((attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_MTT)
+		&& (attr->klm_mtt == NULL || attr->klm_entries == 0)) {
+		snap_error("Provided MTT is not valid\n");
+		return -EINVAL;
+	}
+
+	dv_qp = &q->sw_qp.dv_qp;
+
+	/*
+	 * UMR WQE LAYOUT:
+	 * -----------------------------------------------------------------------
+	 * | gen_ctrl | umr_ctrl | mkey_ctx | inline klm mtt | inline crypto bsf |
+	 * -----------------------------------------------------------------------
+	 *   16bytes    48bytes    64bytes   num_mtt*16 bytes      64 bytes
+	 *
+	 * Note: size of inline klm mtt should be aligned to 64 bytes.
+	 */
+	wqe_size = sizeof(*gen_ctrl) + sizeof(*umr_ctrl) + sizeof(*mkey);
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_MTT) {
+		translation_size = SNAP_ALIGN_CEIL(attr->klm_entries, 4);
+		inline_klm_size = translation_size * sizeof(union mlx5_wqe_umr_inline_seg);
+		wqe_size += inline_klm_size;
+	}
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_CRYPTO_BSF)
+		wqe_size += sizeof(struct snap_crypto_bsf_seg);
+
+	*umr_wqe_n_bb = round_up(wqe_size, MLX5_SEND_WQE_BB);
+
+	/*
+	 * umr wqe only do the modification to mkey,
+	 * and there will be one RDMA/GGA-READ/WEIR wqe
+	 * followed right after to use this modified mkey.
+	 *
+	 * A read or write call consider process succeed
+	 * only when both umr wqe and RDMA/GGA-READ/WEIR
+	 * wqe post succeed.
+	 *
+	 * In order to avoid post UMR WQE succeed but lack
+	 * of tx available resource to post followed DMA WQE
+	 * case, use umr_wqe_n_bb + 1 to do the can_tx check.
+	 */
+	if (snap_unlikely(!qp_can_tx(q, *umr_wqe_n_bb + 1))) {
+		snap_error("Lack of tx_available resource!\n");
+		return -EAGAIN;
+	}
+
+	/*
+	 * sizeof(gen_ctrl) + sizeof(umr_ctrl) == MLX5_SEND_WQE_BB,
+	 * so do not need to worry about wqe buffer warp around.
+	 *
+	 * build genenal ctrl segment
+	 */
+	gen_ctrl = ctrl;
+	snap_set_ctrl_seg(gen_ctrl, dv_qp->hw_qp.sq.pi, MLX5_OPCODE_UMR, 0,
+				dv_qp->hw_qp.qp_num, fm_ce_se,
+				round_up(wqe_size, 16), 0,
+				htobe32(attr->klm_mkey->mkey));
+
+	/* build umr ctrl segment */
+	umr_ctrl = (struct mlx5_wqe_umr_ctrl_seg *)(gen_ctrl + 1);
+	snap_set_umr_ctrl_seg(umr_ctrl, attr);
+	to_end -= MLX5_SEND_WQE_BB;
+
+	/* build mkey context segment */
+	if (to_end == 0) { /* wqe buffer wap around */
+		mkey = (struct mlx5_wqe_mkey_context_seg *)(dv_qp->hw_qp.sq.addr);
+		to_end = dv_qp->hw_qp.sq.wqe_cnt * MLX5_SEND_WQE_BB;
+	} else {
+		mkey = (struct mlx5_wqe_mkey_context_seg *)(umr_ctrl + 1);
+	}
+	snap_set_umr_mkey_seg(mkey, attr);
+	to_end -= MLX5_SEND_WQE_BB;
+
+	inline_mtt_end = NULL;
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_MTT) {
+		klm = (union mlx5_wqe_umr_inline_seg *)(mkey + 1);
+		inline_mtt_end = snap_build_inline_mtt(&to_end, klm, dv_qp, attr);
+	}
+
+	if (attr->purpose & SNAP_UMR_MKEY_MODIFY_ATTACH_CRYPTO_BSF) {
+		if (inline_mtt_end)
+			bsf = (struct snap_crypto_bsf_seg *)inline_mtt_end;
+		else
+			bsf = (struct snap_crypto_bsf_seg *)(mkey + 1);
+		snap_build_inline_crypto_bsf(&to_end, bsf, dv_qp, attr);
+	}
 
 	return 0;
 }
@@ -198,19 +329,10 @@ int snap_umr_post_wqe(struct snap_dma_q *q, struct snap_post_umr_attr *attr,
 	pi = dv_qp->hw_qp.sq.pi & (dv_qp->hw_qp.sq.wqe_cnt - 1);
 	to_end = (dv_qp->hw_qp.sq.wqe_cnt - pi) * MLX5_SEND_WQE_BB;
 
-	switch (attr->purpose) {
-	case SNAP_UMR_MKEY_MODIFY_ATTACH_MTT:
-		ret = build_inline_mtt_umr_wqe(q, ctrl,
-					fm_ce_se, to_end, attr, &umr_wqe_n_bb);
-		break;
-
-	default:
-		snap_error("Not supported post umr wqe operation\n");
-		return -EINVAL;
-	}
-
+	ret = snap_build_umr_wqe(q, ctrl, fm_ce_se, to_end, attr, &umr_wqe_n_bb);
 	if (ret) {
-		snap_error("Failed to build umr wqe for purpose:%d\n", attr->purpose);
+		snap_error("Failed to build umr wqe for purpose:%s\n",
+			attr->purpose == SNAP_UMR_MKEY_MODIFY_ATTACH_MTT ? "attach_mtt" : "attach_bsf");
 		return ret;
 	}
 
--- src/snap_umr.h
+++ src/snap_umr.h
@@ -13,20 +13,84 @@
 #ifndef SNAP_UMR_H
 #define SNAP_UMR_H
 
+#include <linux/types.h>
 #include "snap_dma.h"
 #include "snap_mr.h"
 
+#define SNAP_CRYPTO_KEYTAG_SIZE              8
+#define SNAP_CRYPTO_XTS_INITIAL_TWEAK_SIZE   16
+
 enum {
-	SNAP_UMR_MKEY_MODIFY_ATTACH_MTT = 0x1 << 0,
+	SNAP_UMR_MKEY_MODIFY_ATTACH_MTT        = 0x1 << 0,
+	SNAP_UMR_MKEY_MODIFY_ATTACH_CRYPTO_BSF = 0x1 << 1,
+};
+
+enum {
+	SNAP_CRYPTO_BSF_SIZE_64B  = 0x2,
+};
+
+enum {
+	SNAP_CRYPTO_BSF_P_TYPE_SIGNATURE = 0x0,
+	SNAP_CRYPTO_BSF_P_TYPE_CRYPTO    = 0x1,
+};
+
+enum {
+	SNAP_CRYPTO_BSF_ENCRYPTION_ORDER_ENCRYPTED_WIRE_SIGNATURE   = 0x0,
+	SNAP_CRYPTO_BSF_ENCRYPTION_ORDER_ENCRYPTED_MEMORY_SIGNATURE = 0x1,
+	SNAP_CRYPTO_BSF_ENCRYPTION_ORDER_ENCRYPTED_RAW_WIRE         = 0x2,
+	SNAP_CRYPTO_BSF_ENCRYPTION_ORDER_ENCRYPTED_RAW_MEMORY       = 0x3,
+};
+
+enum {
+	SNAP_CRYPTO_BSF_ENCRYPTION_STANDARD_AES_XTS = 0x0,
+};
+
+enum {
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_RESERVED = 0x0,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_512      = 0x1,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_520      = 0x2,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_4096     = 0x3,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_4160     = 0x4,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_1M       = 0x5,
+	SNAP_CRYPTO_BSF_CRYPTO_BLOCK_SIZE_POINTER_4048     = 0x6,
+};
+
+enum {
+	SNAP_WQE_UMR_CTRL_MKEY_MASK_BSF_OCTOWORD_SIZE = 0x1 << 5,
+};
+
+struct snap_crypto_bsf_seg {
+	uint8_t		size_type;
+	uint8_t		enc_order;
+	uint8_t		rsvd0;
+	uint8_t		enc_standard;
+	__be32		raw_data_size;
+	uint8_t		crypto_block_size_pointer;
+	uint8_t		rsvd1[7];
+	uint8_t		xts_initial_tweak[SNAP_CRYPTO_XTS_INITIAL_TWEAK_SIZE];
+	__be32		dek_pointer;
+	uint8_t		rsvd2[4];
+	uint8_t		keytag[SNAP_CRYPTO_KEYTAG_SIZE];
+	uint8_t		rsvd3[16];
 };
 
 struct snap_post_umr_attr {
-	uint32_t	purpose;
+	uint32_t purpose;
+
+	struct snap_indirect_mkey *klm_mkey;
 
 	/* for attach inline mtt purpose */
-	int				klm_entries;
+	int klm_entries;
 	struct mlx5_klm *klm_mtt;
-	struct snap_indirect_mkey *klm_mkey;
+
+	/* for attach inline crypto bsf purpose*/
+	uint8_t encryption_order;
+	uint8_t encryption_standard;
+	uint32_t raw_data_size;
+	uint8_t crypto_block_size_pointer;
+	uint8_t xts_initial_tweak[SNAP_CRYPTO_XTS_INITIAL_TWEAK_SIZE];
+	uint32_t dek_pointer;
+	uint8_t keytag[SNAP_CRYPTO_KEYTAG_SIZE];
 };
 
 int snap_umr_post_wqe(struct snap_dma_q *q, struct snap_post_umr_attr *attr,
