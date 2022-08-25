2821c3cd50542ef31def9f285b59d74c2ee1bf24
Iaroslav Sydoruk <isydoruk@nvidia.com>
2022-08-25 16:10:13 +0300

    ci: added urm repo
    Signed-off-by: Iaroslav Sydoruk <isydoruk@nvidia.com>

--- .ci/upload.sh
+++ .ci/upload.sh
@@ -1,6 +1,33 @@
 #!/bin/bash -eEx
 
-upload_deb() {
+upload_deb_urm() {
+    # Import gpg key
+    gpg --import ${GPG_KEY_PATH}
+
+    shopt -s nullglob
+
+    deb_pkgs=(${name}*${VER}-${REV}_${arch}*.deb)
+    for deb_pkg in ${deb_pkgs[@]}; do
+        test -e $deb_pkg
+        echo "INFO: Signing package ${deb_pkg##*/}"
+        sign_deb=$(dpkg-sig -k ${gpg_key_name} -c ${deb_pkg}|tail -n1)
+        if [ $sign_deb == "NOSIG" ]; then 
+            dpkg-sig -k ${gpg_key_name} -s builder ${deb_pkg}
+        fi
+        MD5=$(md5sum $deb_pkg | awk '{print $1}')
+        SHA1=$(shasum -a 1 $deb_pkg | awk '{ print $1 }')
+        SHA256=$(shasum -a 256 $deb_pkg | awk '{ print $1 }')
+        upload_url_urm="${REPO_URL}/${name}/${codename}/${STAGE}/${VER}/${deb_pkg};deb.distribution=${codename};deb.component=${repo_name};deb.architecture=${arch}"
+        echo "INFO: Uploading package ${deb_pkg} to ${upload_url_urm}"
+        curl --fail -u "${REPO_USER}:${REPO_PASS}" -X PUT \
+            -H "X-Checksum-MD5:${MD5}" \
+            -H "X-Checksum-Sha1:${SHA1}" \
+            -H "X-Checksum-Sha256:${SHA256}" \
+            -T "${deb_pkg}" "${upload_url_urm}"
+    done
+}
+
+upload_deb_nexus() {
 
     # Import gpg key
     gpg --import ${GPG_KEY_PATH}
@@ -11,10 +38,13 @@ upload_deb() {
     for deb_pkg in ${deb_pkgs[@]}; do
         test -e $deb_pkg
         echo "INFO: Signing package ${deb_pkg##*/}"
-        dpkg-sig -k ${gpg_key_name} -s builder ${deb_pkg}
+        sign_deb=$(dpkg-sig -k ${gpg_key_name} -c ${deb_pkg}|tail -n1)
+        if [ $sign_deb == "NOSIG" ]; then 
+            dpkg-sig -k ${gpg_key_name} -s builder ${deb_pkg}
+        fi
         upload_url="${REPO_URL}/${repo_name}/"
         echo "INFO: Uploading package ${deb_pkg} to ${upload_url}"
-        curl -u "${REPO_USER}:${REPO_PASS}" -H "Content-Type: multipart/form-data" \
+        curl --fail -u "${REPO_USER}:${REPO_PASS}" -H "Content-Type: multipart/form-data" \
             --data-binary "@${deb_pkg}" "${upload_url}"
 
         if test -n "$release_dir" ; then
@@ -25,7 +55,7 @@ upload_deb() {
     done
 }
 
-upload_rpm() {
+upload_rpm_nexus() {
 
     if [ -e /etc/os-release ]; then
         source /etc/os-release
@@ -77,6 +107,58 @@ upload_rpm() {
     done
 }
 
+upload_rpm_urm() {
+
+    if [ -e /etc/os-release ]; then
+        source /etc/os-release
+        if [ $ID = "euleros" ]; then
+            CUSTOM_DIST="euleros"
+        fi
+    fi
+
+    if [ ! -z $CUSTOM_DIST ]; then
+        releasever=$CUSTOM_DIST
+    else
+        releasever=$(rpm --eval "%{rhel}")
+    fi
+
+    shopt -s nullglob
+
+    rpms_location=(${HOME}/rpmbuild/RPMS/${arch}/${name}-*${VER}-${REV}*.rpm)
+    for rpm_location in ${rpms_location[@]}; do
+        MD5=$(md5sum $rpm_location | awk '{print $1}')
+        SHA1=$(sha1sum $rpm_location | awk '{ print $1 }')
+        SHA256=$(sha256sum $rpm_location | awk '{ print $1 }')
+        test -f $rpm_location
+        rpm_name="${rpm_location##*/}"
+        upload_uri="${REPO_URL}/${repo_name}/${releasever}/${arch}/${rpm_name}"
+        echo "INFO: Uploading ${rpm_name} to ${upload_uri}"
+        curl --fail --user "${REPO_USER}:${REPO_PASS}" \
+            -H "X-Checksum-MD5:${MD5}" \
+            -H "X-Checksum-Sha1:${SHA1}" \
+            -H "X-Checksum-Sha256:${SHA256}" \
+            -T $rpm_location -X PUT \
+            ${upload_uri}
+    done
+
+    srpms_location=(${HOME}/rpmbuild/SRPMS/${name}-*${VER}-${REV}*.src.rpm)
+    for srpm_location in ${srpms_location[@]}; do
+        MD5=$(md5sum $srpm_location | awk '{print $1}')
+        SHA1=$(sha1sum $srpm_location | awk '{ print $1 }')
+        SHA256=$(sha256sum $srpm_location | awk '{ print $1 }')
+        test -f $srpm_location
+        srpm_name="${srpm_location##*/}"
+        upload_uri="${REPO_URL}/${repo_name}/${releasever}/SRPMS/${srpm_name}"
+        echo "INFO: Uploading ${srpm_name} to ${upload_uri}"
+        curl --fail --user "${REPO_USER}:${REPO_PASS}" \
+            -H "X-Checksum-MD5:${MD5}" \
+            -H "X-Checksum-Sha1:${SHA1}" \
+            -H "X-Checksum-Sha256:${SHA256}" \
+            -T ${srpm_location} -X PUT \
+            ${upload_uri}
+    done
+}
+
 bd=$(dirname $0)
 user=${USER:-root}
 : ${REPO_URL:?REPO_URL is not found!}
@@ -98,45 +180,60 @@ repo_name="${name}-${VER}"
 if test -n "$ghprbPullId" ; then
     REV="pr${ghprbPullId}"
     repo_name="${repo_name}-pr"
+    STAGE="pr"
 else
     REV=${BUILD_NUMBER:-1}
+    STAGE="release"
 fi
 
 if [[ -f /etc/debian_version ]]; then
 
     codename=$(lsb_release -cs)
-    repo_name="${repo_name}-${codename}-apt"
     arch=$(dpkg --print-architecture)
     : ${GPG_KEY_PATH:? GPG_KEY_PATH is not found!}
     gpg_key_name=$(echo ${GPG_KEY_PATH##*/} | cut -d . -f 1)
 
-    apt update && apt install python3-requests -y
+    if [ $1 == "urm" ]; then
+        REPO_URL="https://urm.nvidia.com/artifactory/sw-nbu-swx-ci-debian-local"
+        upload_deb_urm
+    elif [ $1 == "nexus" ]; then
+        repo_name="${repo_name}-${codename}-apt"
+        apt update && apt install python3-requests -y
 
-    # Create APT repository
-    ${bd}/manage_repo.py apt --name ${repo_name} \
-        --url $( echo $REPO_URL | grep -oP 'http.?://([a-z]+-?\.?)+:?\d+') \
-        --user ${REPO_USER} --password ${REPO_PASS} \
-        --action create --distro ${codename} \
-        --keypair-file ${GPG_KEY_PATH%.*}.priv \
-        --write-policy allow
-
-    upload_deb
+        # Create APT repository
+        ${bd}/manage_repo.py apt --name ${repo_name} \
+            --url $( echo $REPO_URL | grep -oP 'http.?://([a-z]+-?\.?)+:?\d+') \
+            --user ${REPO_USER} --password ${REPO_PASS} \
+            --action create --distro ${codename} \
+            --keypair-file ${GPG_KEY_PATH%.*}.priv \
+            --write-policy allow
+        upload_deb_nexus
+    else
+        echo "Repo not selected"
+        exit 1
+    fi
 
 elif [[ -f /etc/redhat-release ]] || [[ -f /etc/euleros-release ]]; then
 
-    repo_name="${repo_name}-yum"
     arch=$(uname -m)
+    if [ $1 == "urm" ]; then
+        REPO_URL="https://urm.nvidia.com/artifactory/sw-nbu-swx-ci-rpm-local"
+        upload_rpm_urm
+    elif [ $1 == "nexus" ]; then
+        repo_name="${repo_name}-yum"
+        yum install python3-requests -y || yum install python36-requests -y
 
-    yum install python3-requests -y || yum install python36-requests -y
-
-    # Create YUM repository
-    ${bd}/manage_repo.py yum --name ${repo_name} \
-        --url $( echo $REPO_URL | grep -oP 'http.?://([a-z]+-?\.?)+:?\d+') \
-        --user ${REPO_USER} --password ${REPO_PASS} \
-        --repo-data-depth 2 --write-policy allow \
-        --action create
-
-    upload_rpm
+        # Create YUM repository
+        ${bd}/manage_repo.py yum --name ${repo_name} \
+            --url $( echo $REPO_URL | grep -oP 'http.?://([a-z]+-?\.?)+:?\d+') \
+            --user ${REPO_USER} --password ${REPO_PASS} \
+            --repo-data-depth 2 --write-policy allow \
+            --action create
+        upload_rpm_nexus
+    else
+        echo "Repo not selected"
+        exit 1
+    fi
 
 else
 
